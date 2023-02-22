0a968e0cf52fef1a22516a23abdd57766387f831
jfakhary <jamshid.fakhary@tekschool.us>
2023-02-21 19:57:49 -0500

    thisis for push practice

---
+++ src/test/GitPracticeTwo.java
@@ -0,0 +1,18 @@
+package test;
+
+public class GitPracticeTwo {
+
+	public static void main(String[] args) {
+		
+
+		double[] prices = {10.25,3.6,25.12,12.32,14.25,12.5,52};
+		double total = 0;
+		for(int index = 0; index < prices.length; index++) {
+		total += prices[index];	
+		}
+		System.out.println("Total is " + total);
+		
+				
+	}
+
+}
